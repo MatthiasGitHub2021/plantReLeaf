6b382452294ecac3fa92a5fd7fa771ad4fbde4fd
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2022-03-06 21:54:34 -0700

    Made functional
    Commented out some stuff to make program functional

--- plantProject/src/main/java/controller/FormController.java
+++ plantProject/src/main/java/controller/FormController.java
@@ -11,32 +11,30 @@ import beans.User;
 @SuppressWarnings("deprecation")
 @ManagedBean(name="FormController")
 @ViewScoped
-public class FormController 
-{
+public class FormController{
 	FacesContext fc = FacesContext.getCurrentInstance();
 	Map<String, Object> sessionMap = fc.getExternalContext().getSessionMap();
 	
-	public String onLogin(User user){		
-		if(user.getUserName() != sessionMap.get(user.getUserName()) || user.getPassword() != sessionMap.get(user.getPassword())) {
+	public String onLogin(){		
+		//if(user.getUserName() != sessionMap.get(user.getUserName()) || user.getPassword() != sessionMap.get(user.getPassword())) {
 			//add jsf message here
-		}else {
-		return "Login.xhtml";
-		}
+		//}else {
+		return "login.xhtml";
+		//}
 	}
 	
-	public String onRegistration() 
- {		
+	public String onRegistration(){		
 		return "register.xhtml";
  }
 	
-	public String onSubmit(User user) {
-		
-		sessionMap.put("firstName", user.getFirstName());
-		sessionMap.put("lastName", user.getLastName());
-		sessionMap.put("phoneNum", user.getPhoneNum());
-		sessionMap.put("email", user.getEmail());
-		sessionMap.put("userName", user.getUserName());
-		sessionMap.put("password", user.getPassword());
+//	public String onSubmit(User user) {
+	public String onSubmit() {
+//		sessionMap.put("firstName", user.getFirstName());
+//		sessionMap.put("lastName", user.getLastName());
+//		sessionMap.put("phoneNum", user.getPhoneNum());
+//		sessionMap.put("email", user.getEmail());
+//		sessionMap.put("userName", user.getUserName());
+//		sessionMap.put("password", user.getPassword());
 		
 		return "currentEndpoint.xhtml";
 	}
